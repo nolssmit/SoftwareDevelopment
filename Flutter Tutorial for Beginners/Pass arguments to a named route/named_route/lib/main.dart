83e9a783deea4a6297d08bb803c1dbba7b7fabb9
Nols Smit <nols.smit@gmail.com>
2022-09-07 18:10:53 +0200

    WIP

---
+++ Flutter Tutorial for Beginners/Pass arguments to a named route/named_route/lib/main.dart
@@ -0,0 +1,59 @@
+// See: https://docs.flutter.dev/cookbook/navigation/navigate-with-arguments
+
+import 'package:flutter/material.dart';
+
+import './services/screen_arguments.dart';
+import './pages/pass_arguments_screen.dart';
+import './pages/extract_arguments_screen.dart';
+import './pages/home_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        ExtractArgumentsScreen.routeName: (context) =>
+            const ExtractArgumentsScreen(),
+      },
+      // Provide a function to handle named routes.
+      // Use this function to identify the named
+      // route being pushed, and create the correct
+      // Screen.
+      onGenerateRoute: (settings) {
+        // If you push the PassArguments route
+        if (settings.name == PassArgumentsScreen.routeName) {
+          // Cast the arguments to the correct
+          // type: ScreenArguments.
+          final args = settings.arguments as ScreenArguments;
+
+          // Then, extract the required data from
+          // the arguments and pass the data to the
+          // correct screen.
+          return MaterialPageRoute(
+            builder: (context) {
+              return PassArgumentsScreen(
+                title: args.title,
+                message: args.message,
+              );
+            },
+          );
+        }
+        // The code only supports
+        // PassArgumentsScreen.routeName right now.
+        // Other values need to be implemented if we
+        // add them. The assertion here will help remind
+        // us of that higher up in the call stack, since
+        // this assertion would otherwise fire somewhere
+        // in the framework.
+        assert(false, 'Need to implement ${settings.name}');
+        return null;
+      },
+      title: 'Navigation with Arguments',
+      home: const HomeScreen(),
+    );
+  }
+}
