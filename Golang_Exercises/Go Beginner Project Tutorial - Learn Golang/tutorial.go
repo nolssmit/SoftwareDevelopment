f614ab3b8f8e6d70355039f82b57e5757f63bed2
Nols Smit <nols.smit@gmail.com>
2022-12-13 14:47:17 +0200

    WIP Golang 12Dec2022

--- Golang_Exercises/Go Beginner Project Tutorial - Learn Golang/tutorial.go
+++ Golang_Exercises/Go Beginner Project Tutorial - Learn Golang/tutorial.go
@@ -5,11 +5,30 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to my quiz game!")
 
- 	MyName := "Nols"
-	fmt.Printf("Hello I'm %v\n What's your name?\n", MyName)
-	
-	var name string
-	fmt.Scan(&name)
-	fmt.Printf("Hello %v\n,",name)
+	MyName := "Nols"
+	fmt.Printf("Hello I'm %v. What's your name?:", MyName)
 
+	var name string
+	var age uint
+	fmt.Scan(&name)
+	fmt.Printf("Hello %v. How old are you?:", name)
+	fmt.Scan(&age)
+
+	if age > 10 {
+		if age > 60 {
+			fmt.Printf("\nFuck!, %v is the age of a Tabotie tree!!", age)
+		}
+		fmt.Println("\nLet's play!")
+
+	} else {
+		fmt.Println("You are to young to play this game, sorry!")
+		return
+	}
+
+	fmt.Printf("What is better, the RTX 3080 or RTX 3090? ")
+	var answer string
+	var answer2 string
+	fmt.Scan(&answer)
+
+	fmt.Println(answer)
 }
