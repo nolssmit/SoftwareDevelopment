f0130a1c51dea61582840c83bf33c64f41c79a13
Nols Smit <nols.smit@gmail.com>
2023-01-04 07:35:11 +0200

    WIP 4Jan2023

---
+++ Udemy Go Programming Bootcamp/Section 3: Core Go Language/16. Hangman Exercise/gotut-10ex.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+//	"bufio"
+	"fmt"
+//	"log"
+//	"os"
+)
+
+var pl = fmt.Println
+
+/*
+ +----+
+ 0	  |
+/|\	  |
+/ \	  |
+     ===
+
+Secret Word : M_N___
+Incorrect Guesses : A
+
+Guess a Letter : Y
+
+Sorry Your Dead! The word is MONKEY
+Yes the Secret Word is MONKEY
+
+Please Enter Only One Letter
+Please Enter a Letter
+Please Enter a Letter you Haven't Guessed
+
+*/
+
+var hArr = [7] string {
+	" +----+\n" +
+	"      |\n" +
+    "      |\n" +
+    "      |\n" +
+	"     ===\n",
+	" +----+\n" +
+	" o    |\n" +
+    "      |\n" +
+    "      |\n" +
+	"     ===\n",	
+	" +----+\n" +
+	" o    |\n" +
+    " |    |\n" +
+    "      |\n" +
+	"     ===\n",
+	" +----+\n" +
+	" o    |\n" +
+    "/|    |\n" +
+    "      |\n" +
+	"     ===\n",	
+	" +----+\n" +
+	" o    |\n" +
+    "/|\\   |\n" +
+    "      |\n" +
+	"     ===\n",	
+	" +----+\n" +
+	" o    |\n" +
+    "/|\\   |\n" +
+    "/     |\n" +
+	"     ===\n",	
+	" +----+\n" +
+	" o    |\n" +
+    "/|\\   |\n" +
+    "/ \\   |\n" +
+	"     ===\n",					  
+}
+
+var wordArr = [7] string {
+	"JAZZ", "ZIGZAG", "ZILCH", "ZIPPER",
+	"ZODIAC", "ZOMBIE", "FLUFF",
+}
+
+var randWord string
+var guessedLetters string
+var correctLetter []string
+var worongGuesses []string
+
+func main() {
+ //reader := bufio.NewReader(os.Stdin)  //read from keyboard
+
+	// Show Game Board
+
+	// Get a letter from the user
+	// Add to correctLetters
+	// A. If they guessed letter in word
+	//
+	// 1. Are there more letters to guess?
+	// 2. If no more letters to guess (YOU WIN)
+	// B. If they guessed letter not in word
+	// 1. Add new letter to guessedLetters,
+	// wrongGuesses
+	// 2. Check if they died
+	
+
+}
---
+++ Udemy Go Programming Bootcamp/Section 3: Core Go Language/17. Hangman Exercise 2/gotut-10ex.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	//	"bufio"
+	"fmt"
+	"math/rand"
+	"time"
+	// "log"
+	// "os"
+)
+
+var pl = fmt.Println
+
+/*
+ +----+
+ 0	  |
+/|\	  |
+/ \	  |
+     ===
+
+Secret Word : M_N___
+Incorrect Guesses : A
+
+Guess a Letter : Y
+
+Sorry Your Dead! The word is MONKEY
+Yes the Secret Word is MONKEY
+
+Please Enter Only One Letter
+Please Enter a Letter
+Please Enter a Letter you Haven't Guessed
+
+*/
+
+var hArr = [7] string {
+	" +----+\n" +
+	"      |\n" +
+    "      |\n" +
+    "      |\n" +
+	"     ===\n",
+	" +----+\n" +
+	" o    |\n" +
+    "      |\n" +
+    "      |\n" +
+	"     ===\n",	
+	" +----+\n" +
+	" o    |\n" +
+    " |    |\n" +
+    "      |\n" +
+	"     ===\n",
+	" +----+\n" +
+	" o    |\n" +
+    "/|    |\n" +
+    "      |\n" +
+	"     ===\n",	
+	" +----+\n" +
+	" o    |\n" +
+    "/|\\   |\n" +
+    "      |\n" +
+	"     ===\n",	
+	" +----+\n" +
+	" o    |\n" +
+    "/|\\   |\n" +
+    "/     |\n" +
+	"     ===\n",	
+	" +----+\n" +
+	" o    |\n" +
+    "/|\\   |\n" +
+    "/ \\   |\n" +
+	"     ===\n",					  
+}
+
+var wordArr = [7] string {
+	"JAZZ", "ZIGZAG", "ZILCH", "ZIPPER",
+	"ZODIAC", "ZOMBIE", "FLUFF",
+}
+
+var randWord string
+var guessedLetters string
+var correctLetter []string
+var worongGuesses []string
+
+func getRandomWord() {
+	seedSecs := time.Now().Unix()
+	rand.Seed(seedSecs)
+	wordIndex := rand.Intn(7)
+	//pl("wordIndex :", wordIndex)
+	randWord = wordArr[wordIndex]
+	pl(randWord)
+}
+
+func main() {
+ //reader := bufio.NewReader(os.Stdin)  //read from keyboard
+
+	// Show Game Board
+
+	// Get a letter from the user
+	// Add to correctLetters
+	// A. If they guessed letter in word
+	//
+	// 1. Are there more letters to guess?
+	// 2. If no more letters to guess (YOU WIN)
+	// B. If they guessed letter not in word
+	// 1. Add new letter to guessedLetters,
+	// wrongGuesses
+	// 2. Check if they died
+	
+	getRandomWord()
+}
