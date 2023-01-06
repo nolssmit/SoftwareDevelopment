4c134db3337e48217d3d5d293172cb553e6ddb45
Nols Smit <nols.smit@gmail.com>
2023-01-06 17:06:09 +0200

    WIP Laptop 6Jan23

--- Udemy Go Programming Bootcamp/Section 3: Core Go Language/18. Hangman Exercise 3/gotut-10ex.go
+++ Udemy Go Programming Bootcamp/Section 3: Core Go Language/18. Hangman Exercise 3/gotut-10ex.go
@@ -9,92 +9,84 @@ import (
 	"regexp"
 	"strings"
 	"time"
-	// "log"
-	// "os"
 )
 
-var pl = fmt.Println
-
-/*
- +----+
- 0	  |
-/|\	  |
-/ \	  |
-     ===
-
-Secret Word : M_N___
-Incorrect Guesses : A
-
-Guess a Letter : Y
-
-Sorry Your Dead! The word is MONKEY
-Yes the Secret Word is MONKEY
-
-Please Enter Only One Letter
-Please Enter a Letter
-Please Enter a Letter you Haven't Guessed
-
-*/
-
-var hmArr = [7] string {
+var hmArr = [7]string{
 	" +----+\n" +
-	"      |\n" +
-    "      |\n" +
-    "      |\n" +
-	"     ===\n",
+		"      |\n" +
+		"      |\n" +
+		"      |\n" +
+		"     ===\n",
 	" +----+\n" +
-	" o    |\n" +
-    "      |\n" +
-    "      |\n" +
-	"     ===\n",	
+		" o    |\n" +
+		"      |\n" +
+		"      |\n" +
+		"     ===\n",
 	" +----+\n" +
-	" o    |\n" +
-    " |    |\n" +
-    "      |\n" +
-	"     ===\n",
+		" o    |\n" +
+		" |    |\n" +
+		"      |\n" +
+		"     ===\n",
 	" +----+\n" +
-	" o    |\n" +
-    "/|    |\n" +
-    "      |\n" +
-	"     ===\n",	
+		" o    |\n" +
+		"/|    |\n" +
+		"      |\n" +
+		"     ===\n",
 	" +----+\n" +
-	" o    |\n" +
-    "/|\\   |\n" +
-    "      |\n" +
-	"     ===\n",	
+		" o    |\n" +
+		"/|\\   |\n" +
+		"      |\n" +
+		"     ===\n",
 	" +----+\n" +
-	" o    |\n" +
-    "/|\\   |\n" +
-    "/     |\n" +
-	"     ===\n",	
+		" o    |\n" +
+		"/|\\   |\n" +
+		"/     |\n" +
+		"     ===\n",
 	" +----+\n" +
-	" o    |\n" +
-    "/|\\   |\n" +
-    "/ \\   |\n" +
-	"     ===\n",					  
+		" o    |\n" +
+		"/|\\   |\n" +
+		"/ \\   |\n" +
+		"     ===\n",
 }
 
-var wordArr = [7] string {
+var wordArr = [7]string{
 	"JAZZ", "ZIGZAG", "ZILCH", "ZIPPER",
 	"ZODIAC", "ZOMBIE", "FLUFF",
 }
 
+// Stores the random word to be guessed
 var randWord string
+
+// Stores all letters guessed
 var guessedLetters string
+
+// Stores correct guesses
 var correctLetters []string
+
+// Letters guessed that aren't in the randWord
 var wrongGuesses []string
 
 func getRandWord() string {
+	// Returns seconds as int
 	seedSecs := time.Now().Unix()
 	rand.Seed(seedSecs)
+
+	// Get random word from array
 	randWord = wordArr[rand.Intn(7)]
+
+	// Generate correctLetters array with correct size
 	correctLetters = make([]string, len(randWord))
 	return randWord
 }
 
 func showBoard() {
-	fmt.Println(hmArr[len(wrongGuesses)])
+	// Prints current hangman state
+	fmt.Println("Hangman state :", hmArr[len(wrongGuesses)])
+
 	fmt.Print("Secret Word : ")
+
+	// Prints spaces for missing letter or the letter
+	// Cycle and if is a character print it and if not, print an underscore
 	for _, v := range correctLetters {
 		if v == "" {
 			fmt.Print("_")
@@ -113,7 +105,8 @@ func showBoard() {
 
 func getUserLetter() string {
 	// Setup buffered reader that gets text from the keyboard
-  	reader := bufio.NewReader(os.Stdin) 
+	reader := bufio.NewReader(os.Stdin)
+
 	// The guess the user makes
 	var guess string
 
@@ -121,12 +114,18 @@ func getUserLetter() string {
 		fmt.Print("\nGuess a Letter : ")
 		guess, err := reader.ReadString('\n')
 		if err != nil {
-			log.Fatal(err)	
+			log.Fatal(err)
 		}
+
+		// Convert to upper so it is easier to search
 		guess = strings.ToUpper(guess)
+
+		// Remove spaces
 		guess = strings.TrimSpace(guess)
 
+		// Create regular expression that verifies if a single character is a letter
 		var IsLetter = regexp.MustCompile(`^[a-zA-Z]+$`).MatchString
+
 		if len(guess) != 1 {
 			fmt.Println("Please Enter Only One Letter")
 		} else if !IsLetter(guess) {
@@ -136,32 +135,87 @@ func getUserLetter() string {
 		} else {
 			return guess
 		}
-		return guess
 	}
 	return guess
 }
 
+// Returns all indices matching the substring
+func getAllIndexes(theStr, subStr string) (indices []int) {
+	if (len(subStr) == 0) || (len(theStr) == 0) {
+		return indices
+	}
 
+	// Offset will find 1st matching index and then move forward to find the rest
+	offset := 0
+	for {
+		i := strings.Index(theStr[offset:], subStr)
 
+		// If no matches or no more matches since we cycled to the end
+		if i == -1 { // no more matches
+			return indices
+		}
+		offset += i
+
+		// Add index
+		indices = append(indices, offset)
+
+		// Jump ahead the length of the substring
+		offset += len(subStr)
+	}
+}
+
+// Updates correctLetters string to display guessed letters an _s
+func updateCorrectLetters(letter string) {
+	indexMatches := getAllIndexes(randWord, letter)
+
+	// Assign all matching indexes the supplied letter
+	for _, v := range indexMatches {
+		correctLetters[v] = letter
+	}
+}
+
+// Checks if array contains an empty value
+func sliceHasEmptys(theSlice []string) bool {
+	for _, v := range theSlice {
+		if len(v) == 0 {
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
-	fmt.Println(getRandWord())
- 
-	// Show Game Board
-	showBoard()
+	fmt.Println("The random word is ", getRandWord())
 
-	// Get a letter from the user
+	for true {
+		// Display opening board
+		showBoard()
 
-	// Add to correctLetters
+		// Get user guess
+		guess := getUserLetter()
 
-	// A. If they guessed letter in word
-	// Add to correctLetter
-	// 1. Are there more letters to guess?
-	// 2. If no more letters to guess (YOU WIN)
+		if strings.Contains(randWord, guess) {
+			updateCorrectLetters(guess)
 
-	// B. If they guessed letter not in word
-	// 1. Add new letter to guessedLetters, wrongGuesses
-	// 2. Check if they died
-	
+			// Check if user guessed all letters
+			if sliceHasEmptys(correctLetters) {
+				fmt.Println("More letters to Guess")
+			} else {
+				fmt.Println("Yes the secret word is", randWord)
+				break
+			}
+		} else {
+			// They guessed a letter not in the secret word and append to string
+			guessedLetters += guess
 
+			// Append to slice
+			wrongGuesses = append(wrongGuesses, guess)
+
+			// Check if users ran out of guesses
+			if len(wrongGuesses) >= 6 {
+				fmt.Println("Sorry You'r Dead! The word is", randWord)
+				break
+			}
+		}
+	}
 }
